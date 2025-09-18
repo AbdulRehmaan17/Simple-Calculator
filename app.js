a6ff7ef177d5e9702c3dcf1de78bf6e5b83375c6
Abdul Rehman <abdulrehman396387@gmail.com>
2025-09-18 17:18:05 +0500

    new commit

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 let result = document.getElementById("result");
 let buttons = document.querySelectorAll(".btnStyle");
+let lastPressed = false;
 
 buttons.forEach(function(button) {
     button.addEventListener("click", function() {
+
         let value = this.getAttribute("data-value");
-        if (value === "AC"){
+        if(result.value === "Error" || result.value === "0"){
             result.value = "";
+            lastPressed = false;
+        }
+        if (value === "AC"){
+            result.value = "0";
+            lastPressed = false;
         }
         if (value === "=") {
-            result.value = eval(result.value);
+            try {
+                result.value = eval(result.value);
+                lastPressed = true;
+            }
+            catch (error) {
+                result.value = "Error";
+                lastPressed = false;
+            }
         }
         if (value !== "AC" && value !== "="){
             result.value += value;
-        }    
+            lastPressed =true
+        }
+        
     });
 
 });
